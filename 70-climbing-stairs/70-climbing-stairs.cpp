05138cd8dc7d5180580277051910b37ba8540773
Vivek Jha <vijha78@gmail.com>
2022-01-10 23:21:28 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.38%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -3,14 +3,13 @@ public:
     
     int climbStairs(int n) {
         if(n<=2)return n;
-        vector<int>dp(50,0);
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
+        int a=1,b=2,c;
         for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            c=a+b;
+            a=b;
+            b=c;
         }
-        return dp[n];
+        return c;
         
     }
 };
