62568b4b638bedc278f7399ef15acf3b3ef1f629
Vivek Jha <vijha78@gmail.com>
2022-01-10 23:20:08 +0530

    Time: 2 ms (13.91%), Space: 6 MB (53.15%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int func(int n,vector<int>&dp){
-        if(n<=2)return n;
-        if(dp[n]!=0)return dp[n];
-        return dp[n]=func(n-2,dp)+func(n-1,dp);
-    }
+    
     int climbStairs(int n) {
         if(n<=2)return n;
         vector<int>dp(50,0);
         dp[0]=0;
         dp[1]=1;
         dp[2]=2;
-        return func(n,dp);
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
         
     }
 };
