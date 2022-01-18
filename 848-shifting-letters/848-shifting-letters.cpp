539d796df26f540e00c12e30acc78f68a501657c
Vivek Jha <vijha78@gmail.com>
2022-01-18 22:08:54 +0530

    Time: 201 ms (23.21%), Space: 67.7 MB (95.01%) - LeetHub

--- 848-shifting-letters/848-shifting-letters.cpp
+++ 848-shifting-letters/848-shifting-letters.cpp
@@ -3,12 +3,9 @@ public:
     string shiftingLetters(string s, vector<int>& shifts) {
         int n=s.size();
         for(int i=n-2;i>=0;i--)shifts[i]=(shifts[i]+shifts[i+1])%26;
-        string res="";
         for(int i=0;i<n;i++){
-            int ch=s[i]-'a';
-            ch=(ch+shifts[i])%26;
-            res.push_back(ch+'a');
+            s[i]=(s[i]-'a'+shifts[i])%26+'a';
         }
-        return res;
+        return s;
     }
 };
