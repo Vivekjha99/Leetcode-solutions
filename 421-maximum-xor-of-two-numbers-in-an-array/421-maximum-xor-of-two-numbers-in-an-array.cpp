2af055f465811364e0334dedb91cf175212d3981
Vivek Jha <vijha78@gmail.com>
2022-01-28 20:18:24 +0530

    Time: 312 ms (64.77%), Space: 65.2 MB (82.84%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,45 +1,40 @@
-class TrieNode{
-public:
-    TrieNode *child[2];
-    
-    TrieNode(){
-        this->child[0] = NULL; //for 0 bit 
-        this->child[1] = NULL; //for 1 bit
-    }
+struct trienode{
+    trienode* child[2];
 };
 class Solution {
-    TrieNode *newNode;
-    
-    void insert(int x){   //to insert each element into the Trie
-        TrieNode *t = newNode;
-        bitset<32> bs(x);
-        
-        for(int j=31; j>=0; j--){
-            if(!t->child[bs[j]]) t->child[bs[j]] = new TrieNode(); //start from the MSB =, move to LSB using bitset
-            t = t->child[bs[j]];
-        }    
-    }
-    
 public:
-    int findMaximumXOR(vector<int>& nums) {
-        newNode = new TrieNode();
-        for(auto &n : nums) insert(n); //insert all the elements into the Trie
-        
-        int ans=0; //Stores the maximum XOR possible so far
-        for(auto n : nums){
-            ans = max(ans, maxXOR(n));  //updates the ans as we traverse the array & compute max XORs at each element.
+    trienode* insert(vector<int>&nums){
+        trienode* root=new trienode();
+        trienode* curr;
+        for(auto num:nums){
+            int val=num;curr=root;
+            for(int j=31;j>=0;j--){
+                int idx=(val>>j)&1;
+                if(!curr->child[idx])curr->child[idx]=new trienode();
+                curr=curr->child[idx];
+            }
         }
-        return ans;
+        return root;
     }
-    
-    int maxXOR(int n){
-        TrieNode *t = newNode;
-        bitset<32> bs(n);
-        int ans=0; 
-        for(int j=31; j>=0; j--){
-            if(t->child[!bs[j]]) ans += (1<<j), t = t->child[!bs[j]]; //Since 1^0 = 1 & 1^1 = 0, 0^0 = 0
-           
-            else t = t->child[bs[j]];
+    int helper(trienode* curr,int n){
+        int res=0;
+        for(int i=31;i>=0;i--){
+            int idx=(n>>i)&1;
+            idx=1-idx;
+            if(curr->child[idx]){
+                res+=1<<i;
+                curr=curr->child[idx];
+            }
+            else
+                curr=curr->child[1-idx];
+        }
+        return res;
+    }
+    int findMaximumXOR(vector<int>& nums) {
+        trienode* root=insert(nums);
+        int ans=0;
+        for(auto x:nums){
+            ans=max(ans,helper(root,x));
         }
         return ans;
     }
