3b8717f6a942ee02df8a5fbc2b71596800ffe7c8
Vivek Jha <vijha78@gmail.com>
2022-02-12 12:03:49 +0530

    Time: 30 ms (49.72%), Space: 15.8 MB (12.60%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    private:
+    unordered_map<int,vector<int>>g;
+    void make_graph(vector<vector<int>>pre){
+        for(auto x:pre){
+            g[x[1]].push_back(x[0]);
+        }
+    }
+    vector<int>findindegree(int num){
+        vector<int>res(num,0);
+        for(int i=0;i<num;i++){
+            for(auto x:g[i])res[x]++;
+        }
+        return res;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        make_graph(prerequisites);
+        vector<int>ind=findindegree(numCourses);
+        vector<int>res;
+        for(int i=0;i<numCourses;i++){
+           if(ind[i]==0)res.push_back(i); 
+        }
+        for(int i=0;i<res.size();i++){
+            for(auto x:g[res[i]]){
+                ind[x]--;
+                if(ind[x]==0)res.push_back(x);
+            }
+        }
+        return res.size()==numCourses;
+    }
+};
