23bd69b6c504b84e2502c2cb1f4c0817ab3976e1
Vivek Jha <vijha78@gmail.com>
2022-05-30 20:26:45 +0530

    Time: 78 ms (17.41%), Space: 18.6 MB (46.45%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
-    int solve(string s,string t,int i,int j,vector<vector<int>>&dp){
-        if(j<0)return 1;
-        if(i<0)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s[i]==t[j]){
-            int take=solve(s,t,i-1,j-1,dp);
-            int nottake=solve(s,t,i-1,j,dp);
-            return dp[i][j]=take+nottake;
-        }
-        else return dp[i][j]=solve(s,t,i-1,j,dp);
-    }
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
         int mod=1e9+7;
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        // return solve(s,t,n-1,m-1,dp);
         for(int i=0;i<=n;i++)dp[i][0]=1;
         for(int i=1;i<=m;i++)dp[0][i]=0;
         for(int i=1;i<=n;i++){
