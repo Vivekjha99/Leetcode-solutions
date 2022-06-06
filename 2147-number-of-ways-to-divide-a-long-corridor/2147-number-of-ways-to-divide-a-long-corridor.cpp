f98e7b561b342850b3edb238deb829e4439abcef
Vivek Jha <vijha78@gmail.com>
2022-06-06 11:20:49 +0530

    Time: 1807 ms (5.12%), Space: 402.9 MB (5.12%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     int mod=1e9+7;
     vector<vector<int>>dp;
-    int solve(string &s,int i,int k,vector<vector<int>>&dp){
+    int solve(string &s,int i,int k){
         if(i>=s.size())return k==2;
         if(dp[i][k]!=-1)return dp[i][k];
         int ways=0;
         if(k==2){
-            if(s[i]=='P')ways=(solve(s,i+1,k,dp)%mod+solve(s,i+1,0,dp)%mod)%mod;
-            else ways=solve(s,i+1,1,dp)%mod;
+            if(s[i]=='P')ways=(solve(s,i+1,k)%mod+solve(s,i+1,0)%mod)%mod;
+            else ways=solve(s,i+1,1)%mod;
         }
         else{
-            ways=solve(s,i+1,k+(s[i]=='S'),dp)%mod;
+            ways=solve(s,i+1,k+(s[i]=='S'))%mod;
         }
         return dp[i][k]=ways%mod;
     }
     int numberOfWays(string s) {
         int n=s.size();
         dp.resize(n,vector<int>(3,-1));
-        return solve(s,0,0,dp)%mod;
+        return solve(s,0,0)%mod;
     }
     
 };
