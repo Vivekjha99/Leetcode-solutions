26f885605ab7cf5c1f8be295e95f6f93961740e0
Vivek Jha <vijha78@gmail.com>
2022-08-23 11:21:23 +0530

    Time: 26 ms (39.50%), Space: 10.1 MB (41.01%) - LeetHub

--- 719-find-k-th-smallest-pair-distance/719-find-k-th-smallest-pair-distance.cpp
+++ 719-find-k-th-smallest-pair-distance/719-find-k-th-smallest-pair-distance.cpp
@@ -8,7 +8,7 @@ public:
             int mid=(l+r)/2;
             int cnt=0;
             for(int i=0,j=0;i<n;i++){
-                while(j<n && (nums[j]-nums[i])<=mid)j++;
+                while(j<n && abs(nums[j]-nums[i])<=mid)j++;
                 cnt+=j-i-1;
             }
             if(cnt<k)l=mid+1;
