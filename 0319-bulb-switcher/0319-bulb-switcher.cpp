6069f6b50e531ebf60b2ba0d10278e40b3c6355a
Vivek Jha <vijha78@gmail.com>
2024-11-07 22:37:35 +0530

    0319-BULB-SWITCHER Stats: Time: 0 ms (100%), Space: 7.3 MB (74.48%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
+    int findSqrt(int n){
+        int l=1,r=n;long long ans=0;
+        while(l<=r){
+            long long mid=l+(r-l)/2;
+            long res=mid*mid;
+            if(res==n){
+                return mid;
+            }
+            if(mid*mid<n){
+                l=mid+1;
+                ans=mid;
+            }
+            else
+            r=mid-1;
+        }
+        return ans;
+    }
     int bulbSwitch(int n) {
-        return sqrt(n);
+        if(n==0)return 0;
+        return findSqrt(n);
     }
 };
