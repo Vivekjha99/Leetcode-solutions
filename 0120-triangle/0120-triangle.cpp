97b93070561107bdeb0d31a59853f504e2dc24c4
Vivek Jha <vijha78@gmail.com>
2024-11-24 20:21:26 +0530

    0120-TRIANGLE Stats: Time: 0 ms (100%), Space: 11.3 MB (89.96%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-    long getMinSum(vector<vector<int>>&arr,int i,int j,int n,vector<vector<int>>&dp){
-        if(i>=n)return INT_MAX;
-        if(i==n-1)return arr[i][j];
-        if(dp[i][j]!=-1)return dp[i][j];
-        long res=arr[i][j]+min(getMinSum(arr,i+1,j,n,dp),getMinSum(arr,i+1,j+1,n,dp));
-        return dp[i][j]=res;
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>>dp(n+1);
-        for(int i=0;i<n;i++){
-            dp[i].resize(triangle[i].size(),-1);
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<triangle[i].size();j++){
+                triangle[i][j]+=min(triangle[i+1][j+1],triangle[i+1][j]);
+            }
         }
-        return getMinSum(triangle,0,0,n,dp);
+        return triangle[0][0];
     }
 };
