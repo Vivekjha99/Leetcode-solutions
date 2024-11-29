ae68064e1032520079b7ae7146c9eb1c8e306d45
Vivek Jha <vijha78@gmail.com>
2024-11-29 23:36:18 +0530

    0803-CHEAPEST-FLIGHTS-WITHIN-K-STOPS Stats: Time: 166 ms (5.03%), Space: 54.2 MB (5.01%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,35 +1,14 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
-          // create adjacency list
-        unordered_map<int,vector<pair<int, int>>> adjList;
-        for( auto f : flights )
-            adjList[f[0]].push_back( { f[1], f[2] } );
-        
-        // minHeap based on cost of distance from source
-        priority_queue< vector<int>, vector<vector<int>>, greater<vector<int>> > minHeap;
-        minHeap.push( { 0, src, 0 } ); // cost, vertex, hops
-        
-        vector<int> dist(n+1, INT_MAX); //to avoid TLE
-        
-        while( !minHeap.empty() ) {
-            auto t = minHeap.top(); minHeap.pop();
-            int cost = t[0];
-            int curr = t[1];
-            int stop = t[2];
-            
-            if( curr == dst )
-                return cost;
-            
-                //Optimization to avoid TLE
-            if(dist[curr]<stop) continue;
-                 dist[curr]=stop;
-
-            if(stop >K ) continue;
-            
-                for( auto next : adjList[curr] )
-                    minHeap.push( { cost+next.second, next.first, stop+1 });
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<int>>dp(k+2,vector<int>(n,1e9));
+        dp[0][src]=0;
+        for(int i=1;i<=k+1;i++){
+            dp[i][src]=0;
+            for(auto f:flights){
+                dp[i][f[1]]=min(dp[i][f[1]],dp[i-1][f[0]]+f[2]);
+            }
         }
-        return -1;
+        return dp[k+1][dst]==1e9?-1:dp[k+1][dst];
     }
 };
