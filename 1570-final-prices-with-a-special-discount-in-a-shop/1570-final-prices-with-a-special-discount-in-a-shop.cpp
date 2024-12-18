15d01ff00b7bb7b7c8cf168fb806678f4b9dc09b
Vivek Jha <vijha78@gmail.com>
2024-12-18 19:15:21 +0530

    1570-FINAL-PRICES-WITH-A-SPECIAL-DISCOUNT-IN-A-SHOP Stats: Time: 0 ms (100%), Space: 14.2 MB (17.75%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -3,16 +3,18 @@ public:
     vector<int> finalPrices(vector<int>& prices) {
         int n=prices.size();
         vector<int>ans(n);
-        for(int i=n-2;i>=0;i--){
-            ans[i]=prices[i];
-            for(int j=i+1;j<n;j++){
-                if(prices[i]>=prices[j]){
-                    ans[i]=prices[i]-prices[j];
-                    break;
-                }
-            }
-        }
+        stack<int>st;
+        st.push(prices[n-1]);
         ans[n-1]=prices[n-1];
+        for(int i=n-2;i>=0;i--){
+            while(!st.empty() && st.top()>prices[i]){
+                st.pop();
+            }
+            if(!st.empty())
+            ans[i]=prices[i]-st.top();
+            else ans[i]=prices[i];
+            st.push(prices[i]);
+        }
         return ans;
     }
 };
