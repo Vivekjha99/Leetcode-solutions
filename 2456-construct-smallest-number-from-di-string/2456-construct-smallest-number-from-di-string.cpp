e77f0d60a1309dd63e49752afa37d5055507c970
Vivek Jha <vijha78@gmail.com>
2024-12-28 15:52:24 +0530

    Time: 0 ms (100%), Space: 8.4 MB (23.69%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,53 +1,45 @@
+
 class Solution {
-public:
-    void findSmallestNo(string &p,vector<int>&vis,int idx,string &s,string &res){
-        if(idx==p.length()){
-            if(res>s)res=s;
-            return;
+    
+    int ans = INT_MAX;
+    
+    bool solve(int x, int ind , string pattern,vector<int> &vis){        
+        if(ind==pattern.size()){      
+              ans = x; //if we come to the last index it is means we get one possible ans and it is the minimiun 
+              return true;
         }
-        if(idx==-1){
-            for(int j=1;j<=9;j++){
-                if(vis[j]==0){
-                    vis[j]=1;
-                    s+=j+'0';
-                    findSmallestNo(p,vis,idx+1,s,res);
-                    s.pop_back();
-                    vis[j]=0;
+
+       int mini = INT_MAX;
+       int digit = x %10; //last digit 
+
+        if(pattern[ind]=='I'){          
+            for(int i=digit+1 ;i<=9 ;i++){                               
+                if(vis[i]==0){         
+                 vis[i] = 1;
+                 if(mini, solve( x*10 + i,ind+1,pattern,vis))return true;
+                 vis[i] = 0;
+                }
+            }           
+        }
+        else{            
+            for(int i=digit-1;i>=1 ;i--){
+                if(vis[i]==0){
+                 vis[i] = 1;
+                if(solve( x*10+i , ind+1, pattern,vis) ==true)return true;
+                 vis[i] = 0;
                 }
             }
         }
-        else{
-            if(p[idx]=='D'){
-                int t=s[s.length()-1]-'0';
-                for(int j=t-1;j>=1;j--){
-                    if(vis[j]==0){
-                        vis[j]=1;
-                        s+=j+'0';
-                        findSmallestNo(p,vis,idx+1,s,res);
-                        s.pop_back();
-                        vis[j]=0;
-                    }
-                }
-            }
-            else{
-                int t=s[s.length()-1]-'0';
-                for(int j=t+1;j<=9;j++){
-                    if(vis[j]==0){
-                        vis[j]=1;
-                        s+=j+'0';
-                        findSmallestNo(p,vis,idx+1,s,res);
-                        s.pop_back();
-                        vis[j]=0;
-                    }
-                }
-            }
-        }
+        return  false;
     }
-    string smallestNumber(string pattern) {
-        string res="987654321";
-        string s="";
-        vector<int>vis(10,0);
-        findSmallestNo(pattern,vis,-1,s,res);
-        return res;
+public:
+    string smallestNumber(string pattern) {        
+        vector<int> vis(10,0); // keep tracking which digit all ready taken as we cannot take same digit multiple times
+        for(int i=1;i<=9;i++){     
+            vis[i] = 1;
+            if(solve(i,0,pattern,vis)==true)break;//if we get our ans there is not need to go further because it is the minimum 
+            vis[i] = 0;          
+        }
+        return to_string(ans);        
     }
 };
