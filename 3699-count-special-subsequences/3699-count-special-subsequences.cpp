f2fe1fd700503e5f0b9ef72911c62760dbadb53b
Vivek Jha <vijha78@gmail.com>
2024-12-29 13:38:41 +0530

    Time: 635 ms (100%), Space: 94.3 MB (40%) - LeetHub

--- 3699-count-special-subsequences/3699-count-special-subsequences.cpp
+++ 3699-count-special-subsequences/3699-count-special-subsequences.cpp
@@ -5,11 +5,14 @@ public:
         long long res = 0;
         unordered_map<double, int> cnt;
         for (int r = 3; r < n - 2; ++r) {
+            //we are fixing q to r so that we can check all possibilites of r without running a loop for it , just wow.
             int q = r - 2;
-            for (int p = 0; p < q - 1; ++p)
+            for (int p = 0; p < q - 1; ++p){
                 cnt[1.0 * A[p] / A[q]]++;
-            for (int s = r + 2; s < n; ++s)
+            }
+            for (int s = r + 2; s < n; ++s){
                 res += cnt[1.0 * A[s] / A[r]];
+            }
         }
         return res;
     }
