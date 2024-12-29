4db729ae2b3f57425e5078ce0c03eee08a13265e
Vivek Jha <vijha78@gmail.com>
2024-12-29 16:20:46 +0530

    Time: 133 ms (44.58%), Space: 106.1 MB (6.88%) - LeetHub

--- 2305-append-k-integers-with-minimal-sum/2305-append-k-integers-with-minimal-sum.cpp
+++ 2305-append-k-integers-with-minimal-sum/2305-append-k-integers-with-minimal-sum.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     long long minimalKSum(vector<int>& nums, int k) {
-       int n=nums.size();
-       long long res=1ll*k*(k+1)/2;int cnt=0;
-       int ele=k+1;
-       sort(nums.begin(),nums.end());
-       for(int i=0;i<n;i++){
-        while(i>0 && i<n &&   nums[i]==nums[i-1])i++;
-        if(i>=n)break;
-        if(nums[i]<=ele){
-            cout<<nums[i]<<" ";
-            res-=nums[i];
-            res+=ele++;
+        int n = nums.size();
+        long long res = 1ll * k * (k + 1) / 2;
+        set<int> st(nums.begin(), nums.end());
+        for (auto x : st) {
+            if (x <= k) {
+                res +=(++k)-x ;
+            }
         }
-       } 
-       return res;
+        return res;
     }
 };
