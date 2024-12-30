4b9ba97c116f99bf71a54634a4ee6252316d51d3
Vivek Jha <vijha78@gmail.com>
2024-12-30 11:34:12 +0530

    Time: 7 ms (84.37%), Space: 29.2 MB (9.68%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,25 +1,52 @@
 class Solution {
 public:
-    void countPerms(int n, int curr, vector<bool>& placed, int& res) {
-        if (curr > n) {
-            // we have completed one permutation
-            res++;
-            return;
-        }
-        for (int i = 1; i <= n; i++) {
-            if (!placed[i] && (curr % i == 0 || i % curr == 0)) {
-                placed[i] = true;
-                countPerms(n, curr + 1, placed, res);
-                placed[i] = false;
-            }
-        }
-    }
-    int countArrangement(int n) {
-        if (n < 4)
-            return n;
-        vector<bool> placed(n + 1, false);
-        int res = 0;
-        countPerms(n, 1, placed, res);
-        return res;
-    }
+    // void countPerms(int n, int curr, vector<bool>& placed, int& res) {
+    //     if (curr > n) {
+    //         // we have completed one permutation
+    //         res++;
+    //         return;
+    //     }
+    //     for (int i = 1; i <= n; i++) {
+    //         if (!placed[i] && (curr % i == 0 || i % curr == 0)) {
+    //             placed[i] = true;
+    //             countPerms(n, curr + 1, placed, res);
+    //             placed[i] = false;
+    //         }
+    //     }
+    // }
+    // int countArrangement(int n) {
+    //     if (n < 4)
+    //         return n;
+    //     vector<bool> placed(n + 1, false);
+    //     int res = 0;
+    //     countPerms(n, 1, placed, res);
+    //     return res;
+    // }
+    int getBit(int num,int pos){
+	return num& 1<<pos;
+}
+
+int setBit(int num,int pos){
+	return num | 1<<pos;
+}
+int countBeautifulArray(int pos, int mask, vector<vector<int>>& dp,int N){
+	if(pos == N){
+	return 1;
+}
+if(dp[pos][mask]!=-1)
+	return dp[pos][mask];
+int ans =0;
+for(int i = 0; i< N; i++){
+	if(getBit(mask,i)==0){
+		if((i+1)%(pos+1)==0  ||(pos+1)%(i+1)==0)
+		ans += countBeautifulArray(pos+1, setBit(mask,i), dp,N);
+}
+}
+return  dp[pos][mask] = ans;
+}
+int countArrangement(int N) {
+        int mask = 1<<15;
+        vector<vector<int>> dp (N, vector<int>(mask,-1));
+        return countBeautifulArray(0,0,dp,N);    
+}
 };
