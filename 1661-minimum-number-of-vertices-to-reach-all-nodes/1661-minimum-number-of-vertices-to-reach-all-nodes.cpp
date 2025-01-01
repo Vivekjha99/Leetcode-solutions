3be7122937826e28d00a1fdd9f6e75864dce58c6
Vivek Jha <vijha78@gmail.com>
2025-01-01 19:56:52 +0530

    1661-MINIMUM-NUMBER-OF-VERTICES-TO-REACH-ALL-NODES Stats: Time: 48 ms (37.58%), Space: 111.4 MB (28.73%) - LeetHub

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/1661-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/1661-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        unordered_map<int,vector<int>>g;
         vector<int>ind(n,0);
         for(auto e:edges){
-            g[e[0]].push_back(e[1]);
             ind[e[1]]++;
         }
         vector<int>nodes;
-        unordered_set<int>st;
         for(int i=0;i<n;i++){
             if(ind[i]==0)nodes.push_back(i);
         }
