b58eeabb531a1c54a9840d85240d04519c1355f5
Vivek Jha <vijha78@gmail.com>
2025-01-01 23:11:32 +0530

    1706-MIN-COST-TO-CONNECT-ALL-POINTS Stats: Time: 390 ms (50.07%), Space: 46 MB (75.97%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
@@ -1,47 +1,26 @@
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int n = points.size();
-map<pair<int, int>, vector<pair<pair<int, int>, int>>> vec;
-// here i am just maintaining x and y and pushed distance from any other  x1 & y1 from x & y
-for(int i=0;i<n ;i++){
-	for(int j=i+1;j<n;j++){
-		int dis = abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
-		vec[{points[i][0],points[i][1]}].
-		push_back({{points[j][0],points[j][1]},dis});
-		vec[{points[j][0], points[j][1]}].
-		push_back({{points[i][0], points[i][1]}, dis});
-	}
-}
-
-set<pair<int, int>> visi; // to mark visi and also store negative value 
-
-priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,
-greater<pair<int,pair<int,int>>>>q; // to have minimum distance 
-
-q.push({0,{points[0][0],points[0][1]}});
-
-int sum=0;
-
-// standard mst
-while(!q.empty()){
-	int w = q.top().first;
-	int x = q.top().second.first;
-	int y = q.top().second.second;
-
-	q.pop();
-
-	if(visi.count({x,y}))continue; 
-	visi.insert({x, y});
-	sum+=w;
-
-	for(auto ele :vec[{x,y}]){
-		int newx=ele.first.first;
-		int newy=ele.first.second;
-		int adw = ele.second;
-		q.push({adw,{newx,newy}});
-	}
-}
-return sum;
+        int n=points.size();
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        vector<bool>vis(n,false);
+        pq.push({0,0});//dist,index of point
+        int minCost=0;
+        while(!pq.empty()){
+            auto p=pq.top();
+            pq.pop();
+            int wt=p.first;
+            int idx=p.second;
+            if(vis[idx])continue;
+            vis[idx]=true;
+            minCost+=wt;
+            for(int nIdx=0;nIdx<n;nIdx++){
+                if(!vis[nIdx]){
+                    int nWt=abs(points[nIdx][0]-points[idx][0])+abs(points[idx][1]-points[nIdx][1]);
+                    pq.push({nWt,nIdx});
+                }
+            }
+        }
+        return minCost;
     }
 };
