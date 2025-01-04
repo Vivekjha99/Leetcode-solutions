388e7d41cd02fbd752b7dbb1af89bbe84d5c2278
Vivek Jha <vijha78@gmail.com>
2025-01-04 11:20:36 +0530

    2358-NUMBER-OF-WAYS-TO-SPLIT-ARRAY Stats: Time: 10 ms (13.38%), Space: 97.2 MB (21.38%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
         int n=nums.size();
-        vector<long long>pref(n,0),suff(n,0);
-        for(int i=0;i<n;i++){
-            pref[i]=1ll*nums[i];
-            if(i>0)pref[i]+=pref[i-1];
-        }
+        vector<long long>suff(n,0);
         for(int i=n-1;i>=0;i--){
             suff[i]=1ll*nums[i];
             if(i<n-1)suff[i]+=suff[i+1];
         }
-        int res=0;
+        int res=0;long long pref=0;
         for(int i=0;i<n-1;i++){
-            if(pref[i]>=suff[i+1])res++;
+            pref+=nums[i];
+            if(pref>=suff[i+1])res++;
         }
         return res;
     }
